f24f9bc55d65bb230379cf46e5e01971393a1936
kavyangaHA <kavyangahathurusinghe1234@gmail.com>
2026-03-04 23:07:36 +0530

    add a search bar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -44,6 +44,15 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
             </div>
           </div>
         </div>
+        <div> <div className='bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5'>
+  <img src={assets.search_icon} alt="Search" className='w-3' />
+  <input
+    type="text"
+    className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1'
+    placeholder='Search Subscribe'
+  />
+</div></div>
+
       </div>
     </div>
   );
